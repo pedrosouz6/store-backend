1a0e191f51367ec902dcfb64724ab728df39af2d
Pedro Souza <peedrosouz6@gmail.com>
2022-05-01 20:17:38 -0300

    create client

--- controllers/Client/index.js
+++ controllers/Client/index.js
@@ -1,3 +1,4 @@
+const { send } = require('express/lib/response');
 const { connect } = require('../../connection/index');
 
 class ControllersClient {
@@ -27,6 +28,37 @@ class ControllersClient {
                 error: false,
                 message: 'Cliente cadastrado com sucesso'
             })
+        })
+    }
+
+    loginClient(req, res) {
+        const { email, password } = req.body;
+
+        const sql = `SELECT * FROM client WHERE 
+        email_client = '${email}' && 
+        password_client = '${password}'`;
+
+        connect.query(sql, (error, results) => {
+            if(error) {
+                return res.send({
+                    error: true,
+                    message: 'Erro ao identificar o cliente'
+                })
+            }
+
+            if(results.length === 0) {
+                return res.send({
+                    error: true,
+                    message: 'Usuário não existe'
+                })
+            }
+
+            if(results.length === 1) {
+                return res.send({
+                    error: false,
+                    message: 'O usuário existe'
+                })
+            }
         })
     }
 }
--- routes/Client/index.js
+++ routes/Client/index.js
@@ -4,5 +4,6 @@ const express = require('express');
 const router = express.Router();
 
 router.post('/add/client', ControllersClient.createClient);
+router.post('/login/client', ControllersClient.loginClient);
 
 module.exports = router;
